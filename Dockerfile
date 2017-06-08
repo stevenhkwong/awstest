ace233f0c9c3b9f7c47fd8b833ac71839c95e27c
swong <swong@wongsrus.net>
2017-06-08 19:52:41 +1000

    modified:   Dockerfile modified:   i_test.py

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ ADD index.php /var/www/html/
 ADD i_start.py /var/www/html/
 ADD i_destroy.py /var/www/html/
 ADD i_status.py /var/www/html/
+ADD i_test.py /var/www/html/
 
 EXPOSE 80
 
--- i_test.py
+++ i_test.py
@@ -5,30 +5,42 @@ import os
 import sys, getopt
 
 debug = 0
-qty = 0
-line_mode = False
-running_only = False
 default_key = 'XXXXX'
 default_secret = 'XXXXX'
+path = "/var/www/html/" 
+files = [path + "i_destroy.py",   path + "index.php" , path + "i_start.py", path + "i_status.py"  , path + "i_test.py" ]
 argv = None
 
-try:
-  regions = boto.ec2.regions()
-except Exception, e:
-  print "failed to get regions!!\n"
-  sys.exit(2)
-#
-if regions == None:
-  print "no regions!!\n"
-  sys.exit(2)
-
-print "Available regions:"
-for r in regions:
-  print "  - %s" % (r)
-
-
-
-
 def usage():
-  print 'i_test.py [-h -l] [-r -l] -k key -s secret'
-  print '  -h - this help'
+	print 'i_test.py [-h -l] [-r -l] -k key -s secret'
+	print '  -h - this help'
+
+def testaws():
+	try:
+		regions = boto.ec2.regions()
+	except Exception, e:
+		print "failed to get regions!!\n"
+		sys.exit(2)
+	#
+	if regions == None:
+		print "no regions!!\n"
+		sys.exit(2)
+
+	print "Available regions:"
+	for r in regions:
+		print "  - %s" % (r)
+
+def testfiles():
+	print "Files:"
+	for file in files:
+		if not os.path.exists(file):
+			sys.exit(2)
+		print "  - %s ok" % (file)
+			
+		
+testaws()
+testfiles()
+
+
+
+
